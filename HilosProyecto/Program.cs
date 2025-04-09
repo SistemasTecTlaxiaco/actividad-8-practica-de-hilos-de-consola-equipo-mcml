5eab8d931ee2a965176a3898189b63cbee6e8b7d
Mari cruz <Mari cruz@DESKTOP-1772QQO>
2025-04-08 21:19:48 -0600

    Aqui se crearon 5 hilos

--- HilosProyecto/Program.cs
+++ HilosProyecto/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HilosProyecto
@@ -11,6 +12,18 @@ namespace HilosProyecto
     {
         static void Main(string[] args) // // Método principal: punto de entrada del programa
         {
+            Console.WriteLine(" Iniciando proceso de empacado..."); // Muestra mensaje de inicio
+
+            // Crea cinco hilos, ambos ejecutarán el método Empacar
+            Thread empacador1 = new Thread(Empacar);
+            Thread empacador2 = new Thread(Empacar);
+            Thread empacador3 = new Thread(Empacar);
+            Thread empacador4 = new Thread(Empacar);
+            Thread empacador5 = new Thread(Empacar);
+
+
         }
+
     }
+}
 }
